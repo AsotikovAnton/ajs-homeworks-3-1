03a1b0bddf794ea79cd5ba75f9fc6c73e48ec33f
Asotikov Anton <asant@yandex.ru>
2019-12-04 17:47:41 +0300

    Поправил ошибки линтера

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,11 @@
 export default function characterStatus(character) {
   let status = '';
-    if (character.health > 50) {
-      status = 'healthy';
-    } else if (character.health > 14 && character.health < 51) {
-      status = 'wounded';
-    } else if (character.health < 15) {
-      status = 'critical';
-    }
-
+  if (character.health > 50) {
+    status = 'healthy';
+  } else if (character.health > 14 && character.health < 51) {
+    status = 'wounded';
+  } else if (character.health < 15) {
+    status = 'critical';
+  }
   return status;
 }
